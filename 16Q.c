f5cfe67901bb8002f529363e60b00093b8ac5afe
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-09-30 19:33:20 +0530

    Create 16Q.c

---
+++ 16Q.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int i,j,n=5,k=n-1;
+    for(i=0;i<n-1;i++){
+        for(j=0;j<k-i;j++)
+            printf(" ");
+        for(j=1;j<2;j++)
+            printf("*");
+        for(j=1;j<=2*i-1;j++)
+            printf(" ");
+        if(i>0){
+            for(j=0;j<1;j++)
+                printf("*");
+        }
+        printf("\n");
+    }
+    for(i=0;i<2*n-1;i++)
+        printf("*");
+    return 0;
+}
